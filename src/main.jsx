02de63b308ecdad9a89a0d2045ac8f1eab46d1ff
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-02-07 10:16:31 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { LocationProvider } from './context/LocationContext.jsx'
 import { NavbarProvider } from './context/NavbarContext.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LocationProvider>
-      <NavbarProvider>
-        <App />
-      </NavbarProvider>
-    </LocationProvider>
+    <Analytics>
+      <LocationProvider>
+        <NavbarProvider>
+          <App />
+        </NavbarProvider>
+      </LocationProvider>
+    </Analytics>
   </StrictMode>,
 )
