f755e89e59f0b93e7ce28c1f5a7794315e0ff95a
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-02-07 15:40:20 +0700

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { LocationProvider } from './context/LocationContext.jsx'
 import { NavbarProvider } from './context/NavbarContext.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics>
       <LocationProvider>
         <NavbarProvider>
           <App />
+          <Analytics/>
         </NavbarProvider>
       </LocationProvider>
-    </Analytics>
   </StrictMode>,
 )
